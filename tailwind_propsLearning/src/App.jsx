9cff673e5b69d8e13f3dac51a5b333270e4c7dc7
Debasish Ghosh <debasishghosh20032025@gmail.com>
2026-02-12 12:18:08 +0530

    done

---
+++ tailwind_propsLearning/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Card username="camera1"/>
+      <Card username="camera2" />
+    </>
+  )
+}
+
+export default App
