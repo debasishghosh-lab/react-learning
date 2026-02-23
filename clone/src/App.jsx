29acc3815b8d7761399bf0390f78839192c7a0de
Debasish Ghosh <debasishghosh20032025@gmail.com>
2026-02-23 17:44:19 +0530

    routes added

--- clone/src/App.jsx
+++ clone/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <Header />
+      <Header></Header>
+      <Outlet />
+      <Footer></Footer>
     </>
   )
 }
